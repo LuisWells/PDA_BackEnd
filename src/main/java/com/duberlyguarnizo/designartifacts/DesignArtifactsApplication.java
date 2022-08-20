a9f53a351673f4d4831d10ee098b28117b752aea
Duberly Guarnizo <duberlygfr@gmail.com>
2022-08-20 10:51:21 -0500

    Initial commit

---
+++ src/main/java/com/duberlyguarnizo/designartifacts/DesignArtifactsApplication.java
@@ -0,0 +1,13 @@
+package com.duberlyguarnizo.designartifacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignArtifactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignArtifactsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duberlyguarnizo/designartifacts/DesignArtifactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duberlyguarnizo.designartifacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignArtifactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
