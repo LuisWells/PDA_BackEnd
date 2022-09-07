257085b2c1e96874070489595ecb511aee8b4e0f
Duberly Guarnizo <duberlygfr@gmail.com>
2022-09-07 16:16:54 -0500

    Added start-up Sql script to populate ADMIN table. Added CORS to endpoints (still needs to be secured).

--- src/main/java/com/duberlyguarnizo/designartifacts/controller/AdminController.java
+++ src/main/java/com/duberlyguarnizo/designartifacts/controller/AdminController.java
@@ -5,13 +5,11 @@ import com.duberlyguarnizo.designartifacts.repository.AdminRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/admin")
 public class AdminController {
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,4 +3,4 @@ VALUES ('pepe@pepemail.com', 1, 'Pepecito Juarez');
 INSERT INTO admin (email, is_active, name)
 VALUES ('duberlygfr@gmail.com', 1, 'Duberly Guarnizo');
 INSERT INTO admin (email, is_active, name)
-VALUES ('pprofeluchon@altavista.com', 1, 'leono Gutioso');
+VALUES ('profeluchon@altavista.com', 1, 'leono Gutioso');
